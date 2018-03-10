b2e93a64193923e6a695dc160043132016e50f93
MattMorgis <matt@rappiddevelopment.com>
2018-03-09 21:20:00 -0500

    feat: pretty print uptime

--- src/routes/AppRouter.ts
+++ src/routes/AppRouter.ts
@@ -13,9 +13,21 @@ class AppRouter {
     this.router.get(
       '/',
       (request: Request, response: Response, next: NextFunction) => {
-        response.render('index', {pid: process.pid, uptime: process.uptime});
+        response.render('index', {
+          pid: process.pid,
+          uptime: this.format(process.uptime()),
+        });
       }
     );
+  }
+
+  private format(uptime): string {
+    const pad = s => (s < 10 ? '0' : '') + s;
+    const hours = Math.floor(uptime / (60 * 60));
+    const minutes = Math.floor((uptime % (60 * 60)) / 60);
+    const seconds = Math.floor(uptime % 60);
+
+    return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
   }
 }
 
